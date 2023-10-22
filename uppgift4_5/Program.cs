c9671e41f1592e4567eadf5e45620581e3ba7124
Kevin <Kevin@DESKTOP-27L22BB>
2023-10-22 12:10:11 +0200

    Add project files.

---
+++ uppgift4_5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace uppgift4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv in ett heltal");
+            string heltal=Console.ReadLine();
+            string ut = "";
+
+            for (int i = 0; i<heltal.Length;i++)
+            { 
+                string tecken = heltal[i].ToString();
+                try
+                {
+                    int siffra = int.Parse(tecken) + 1;
+                    if (siffra == 10)
+                    {
+                        siffra = 0;
+                    }
+                    ut = ut + siffra;
+                }
+                catch
+                {
+                    Console.WriteLine("tecken är inte en siffra: " + tecken);
+                }
+            }
+            Console.WriteLine(ut);
+            Console.ReadKey();
+        }
+    }
+}
